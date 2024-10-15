b284c8dfedb6b9417984183f9854337edc9557e0
Adam Havlik <xhavli59@stud.fit.vutbr.cz>
2024-10-15 16:41:08 +0200

    feat: Argument parsing and printing available devices implemented sucessfully

--- isa-top.cpp
+++ isa-top.cpp
@@ -1,31 +1,81 @@
+// Comp: g++ isa-top.cpp -o isa-top -lpcap
+// Run: ./isa-top -eno1 0 -s b
 #include <iostream>
-#include <unistd.h>  // For getopt()
+#include <unistd.h> // For getopt()
 #include <string>
 #include <cstring>
-#include <cstdlib>   // For exit()
-#include <stdexcept> // For exception handling
-#include <pcap.h>
+#include <cstdlib>  // For exit()
+#include <stdexcept>    // For exception handling
+#include <vector>       // For std::vector
+#include <map>          // For std::map
+#include <pcap.h>   //Fedora sudo dnf install libpcap-devel
 
 struct Config {
-    int interfaceNumber = -1;
+    std::string interfaceName;
     std::string sortOption;
 };
 
 Config config;
+char errBuff[PCAP_ERRBUF_SIZE];
+pcap_if_t *alldevs;
+pcap_if_t *device;
+std::map<std::string, pcap_if_t*> devicesDictionary;
 
-void print_list_of_available_interfaces(){
-    //TODO
-    std::cout << "  TODO\n"
-              << "  Print available Interfaces\n";
+/**
+ * @brief Store all available devices in the deviceMap vector
+ */
+void process_all_devices(){
+    for (device = alldevs; device != nullptr; device = device->next) {
+        devicesDictionary[device->name] = device;
+    }
+
+    if (devicesDictionary.empty()) {
+        std::cerr << "No devices found." << std::endl;
+        pcap_freealldevs(alldevs);
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
- * @brief Print help
+ * @brief Find all available devices and store them into alldevs variable
+ */
+void find_all_devices(){
+    if (pcap_findalldevs(&alldevs, errBuff) == -1) {
+        std::cerr << "Error finding devices: " << errBuff << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * @brief Print vector of all available interfaces with its description
+ */
+void print_all_interfaces(){
+    find_all_devices();
+    process_all_devices();
+
+    std::cout << "Available interfaces:" << std::endl;
+    int i = 0;
+    for (const auto& [name, device] : devicesDictionary) {
+    std::cout << i + 1 << ". " << device->name;
+    if (device->description) {
+        std::cout << " (" + std::string(device->description) + ")";
+    }
+    std::cout << std::endl;
+    i++;
+    }
+
+    pcap_freealldevs(alldevs);
+}
+
+/**
+ * @brief Print help message
  */
 void print_help() {
-    std::cout << "Usage: isa-top [options]\n"
-              << "Options:\n"
-              << "  -i <int>    Specify the interface number\n"
+    std::cout << "Usage: isa-top -i interface [-s b|p]\n"
+              << "Required:\n"
+              << "  -i          Print all available devices\n"
+              << "  -i <int>    Specify the interface name\n"
+              << "Optional:\n"
               << "  -s <b|p>    Sort by bytes (b) or packets (p)\n"
               << "  -h          Show this help message\n";
 }
@@ -40,25 +90,15 @@ void parse_arguments(int argc, char **argv) {
     while ((option = getopt(argc, argv, "i:s:h")) != -1) {
         switch (option) {
             case 'i': // Interface
-                try {
-                    std::size_t pos;
-                    config.interfaceNumber = std::stoi(optarg, &pos);
-                    if (pos != std::strlen(optarg)) {
-                        std::cerr << "Error: Invalid interface number. Input contains non-integer characters.\n";
-                        exit(EXIT_FAILURE);
-                    }
-                } catch (const std::invalid_argument& e) {
-                    std::cerr << "Error: Invalid interface number (non-integer value)\n";
-                    exit(EXIT_FAILURE);
-                } catch (const std::out_of_range& e) {
-                    std::cerr << "Error: Interface number out of range\n";
-                    exit(EXIT_FAILURE);
-                }
+                config.interfaceName = optarg;
                 break;
 
             case 's': // Sort option
-                config.sortOption = optarg;
-                if (config.sortOption != "b" && config.sortOption != "p") {
+                if (strcmp(optarg, "b") == 0) {
+                    config.sortOption = "bytes";
+                } else if (strcmp(optarg, "p") == 0) {
+                    config.sortOption = "packets";
+                } else {
                     std::cerr << "Error: Invalid value for -s option. Use 'b' for bytes or 'p' for packets\n";
                     exit(EXIT_FAILURE);
                 }
@@ -70,11 +110,11 @@ void parse_arguments(int argc, char **argv) {
 
             case '?': // Unknown or missing argument
                 if (optopt == 'i') {
-                    print_list_of_available_interfaces();
+                    print_all_interfaces();
                     exit(EXIT_SUCCESS);
                 }
                 std::cerr << "Error: Unknown option or missing argument\n";
-                //print_help();
+                print_help();
                 exit(EXIT_FAILURE);
 
             default:
@@ -83,16 +123,17 @@ void parse_arguments(int argc, char **argv) {
         }
     }
 
-    if (config.interfaceNumber == -1) {
+    if (config.interfaceName == "") {
         std::cerr << "Error: -i option is required\n";
         print_help();
         exit(EXIT_FAILURE);
     }
 
-    if (config.sortOption.empty()) {
-        std::cerr << "Error: No sorting option provided\n";
-        exit(EXIT_FAILURE);
-    }
+    //TODO decide what to do with sort option
+    // if (config.sortOption.empty()) {
+    //     std::cerr << "Error: No sorting option provided\n";
+    //     exit(EXIT_FAILURE);
+    // }
 }
 
 
@@ -101,10 +142,21 @@ int main(int argc, char* argv[]) {
 
     parse_arguments(argc, argv);
     
-    std::cout << "Interface: " << config.interfaceNumber << std::endl;
-    std::cout << "Sorting by: " << (config.sortOption == "b" ? "bytes" : "packets") << std::endl;
 
-    char errBuff[PCAP_ERRBUF_SIZE];
+    find_all_devices();
+    process_all_devices();
+
+    pcap_if_t *device = devicesDictionary[config.interfaceName];
+    if (device == nullptr) {
+        std::cerr << "Error: Interface " << config.interfaceName << " not found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Selected interface: " << device->name << std::endl;
+    std::cout << "Description: " << (device->description ? device->description : "N/A") << std::endl;
+    std::cout << "Sorting by: " << config.sortOption << std::endl;
+
+    pcap_freealldevs(alldevs);
 
     exit(EXIT_SUCCESS);
 }
